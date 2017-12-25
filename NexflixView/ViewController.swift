a19230dad3ca76bf28eb5dd346f77523f3264b9a
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2017-12-24 20:32:22 -0500

    Initial Commit

---
+++ NexflixView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NexflixView
+//
+//  Created by Richard Crichlow on 12/24/17.
+//  Copyright © 2017 Richard Crichlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
